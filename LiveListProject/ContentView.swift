a2493a33e25aebe644c4a68a9bab2443054f9fcd
carlosam2 <a01703682@itesm.mx>
2021-05-06 19:18:57 -0500

    Initial Commit

---
+++ LiveListProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiveListProject
+//
+//  Created by Carlos Ayala Medina on 06/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
